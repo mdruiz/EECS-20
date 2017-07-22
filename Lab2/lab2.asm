cd38b64215a135acfc16a44044354980fed51af6
Mario Ruiz <30361667+mdruiz@users.noreply.github.com>
2017-07-21 20:06:23 -0700

    Initial commit

---
+++ Lab2/lab2.asm
@@ -0,0 +1,39 @@
+
+                .ORIG x3000
+
+		AND R0,R0,#0
+
+                AND R1,R1,#0
+		
+		AND R2,R2,#0
+
+		AND R3,R3,#0
+	
+		AND R4,R4,#0
+
+		AND R5,R5,#0
+
+		ADD R0, R0, #0
+		
+		ADD R1, R1, #1 
+
+		ADD R2, R2, #2
+
+		ADD R3, R3, #3
+
+		ADD R4, R4, #4    
+
+		ADD R5, R5, #5
+		
+		ADD R0, R0, R1
+
+		ADD R0, R0, R2
+
+		ADD R0, R0, R3
+
+		ADD R0, R0, R4
+
+		ADD R0, R0, R5
+		
+		.END
+		
